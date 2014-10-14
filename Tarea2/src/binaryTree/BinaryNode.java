e10c524316641e19bd86f24ce28b4de78bb88752
sesanchez <sesanchezr@hotmail.com>
2014-10-14 02:44:20 -0300

    Avances

---
+++ Tarea2/src/binaryTree/BinaryNode.java
@@ -0,0 +1,13 @@
+package binaryTree;
+
+public class BinaryNode {
+
+	int key;
+	BinaryNode left, right;
+	
+	public BinaryNode(int key){
+		this.key = key;
+		left = null;
+		right =null;
+	}
+}
---
+++ Tarea2/src/binaryTree/BinaryTree.java
@@ -0,0 +1,14 @@
+package binaryTree;
+
+public class BinaryTree {
+	
+	BinaryNode root;
+	
+	public BinaryTree(){
+		this.root = null;
+	}
+	
+	public void insert(int data){
+		
+	}
+}
